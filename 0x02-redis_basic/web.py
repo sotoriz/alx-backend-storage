b25c4eb71a11eff888fe170f0c744b3f2f19c35e
sotoriz <sotorisfx@gmail.com>
2023-07-19 17:53:26 -0700

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-""" web.py """
+""" Module for Implementing an expiring web cache and tracker """
 
+from functools import wraps
 import redis
 import requests
 from typing import Callable
-from functools import wraps
 
-rd = redis.Redis()
+client = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """ Checking number of requests has been made """
+    """ Decortator to count how many request has been made"""
 
     @wraps(method)
     def wrapper(url):
-        """ Wrapper function """
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
+        """ Function wrapper """
+        client.incr(f"count:{url}")
+        cached_html = client.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
 
         html = method(url)
-        rd.setex(f"cached:{url}", 10, html)
+        client.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -29,6 +29,7 @@ def count_requests(method: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """ Return html content """
+    """Gets the html content of a web page
+    """
     req = requests.get(url)
     return req.text
