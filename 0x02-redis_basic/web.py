e0fd63007d9114f7e402390e3faccb155fe6df73
sotoriz <sotorisfx@gmail.com>
2023-07-19 17:44:31 -0700

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,51 +1,34 @@
 #!/usr/bin/env python3
-"""web module
-"""
-from functools import wraps
-from typing import Callable
+""" web.py """
+
 import redis
 import requests
+from typing import Callable
+from functools import wraps
 
-_redis = redis.Redis()
-_redis.flushdb()
+rd = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """count_requests function
+    """ Checking number of requests has been made """
 
-    Args:
-        method (Callable): method
-
-    Returns:
-        Callable: wrapper
-    """
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        """wrapper function
+    def wrapper(url):
+        """ Wrapper function """
+        rd.incr(f"count:{url}")
+        cached_html = rd.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-        Returns:
-            [type]: wrapper
-        """
-        url = args[0]
-        cached = _redis.get(f"cached:{url}")
-        if cached:
-            return cached.decode("utf-8")
-        response = method(*args, **kwargs)
-        _redis.incr(f"count:{url}")
-        _redis.setex(f"cached:{url}", 10, response)
-        return response
+        html = method(url)
+        rd.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """get_page function
-
-    Args:
-        url (str): url
-
-    Returns:
-        str: response
-    """
-    response = requests.get(url, timeout=10)
-    return response.text
+    """ Return html content """
+    req = requests.get(url)
+    return req.text
